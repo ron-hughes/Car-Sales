323dd6e21a95074800c6975d8fc473b02ea3b61a
Ron Hughes <ronald.hughes1982@gmail.com>
2020-03-03 09:39:39 -0500

    started skeleton for redux app

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -5,7 +5,7 @@ const AdditionalFeature = props => {
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
       <button className="button">Add</button>
-      {props.feature.name} (+{props.feature.price})
+      {props.feature.name} ({props.feature.price})
     </li>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import { reducer } from './store/reducers'
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+// create store and pass in reducer i.e STORE = Global state
+const store = createStore(reducer);
+
+// Create provider and pass in store prop for entire APP.
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+<Provider store={store}><App /></Provider>, rootElement);
